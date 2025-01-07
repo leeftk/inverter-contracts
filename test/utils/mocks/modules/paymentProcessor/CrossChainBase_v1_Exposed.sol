e17ae529797d6b526039a772d0019cc7b131786f
Leandro Faria <lee.lara1219@gmail.com>
2025-01-06 22:46:42 -0600

    add base test file

--- test/utils/mocks/modules/paymentProcessor/CrossChainBase_v1_Exposed.sol
+++ test/utils/mocks/modules/paymentProcessor/CrossChainBase_v1_Exposed.sol
@@ -1,15 +1,12 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 
 // Internal Dependencies
-//import {PP_CrossChain_v1} from "src/templates/modules/PP_Template_v1.sol";
 import {CrossChainBase_v1} from
     "src/modules/paymentProcessor/abstracts/CrossChainBase_v1.sol";
 import {IERC20PaymentClientBase_v1} from
     "@lm/interfaces/IERC20PaymentClientBase_v1.sol";
 
 contract CrossChainBase_v1_Exposed is CrossChainBase_v1 {
-    ///// @inheritdoc CrossChainBase_v1
-
     function _executeBridgeTransfer(
         IERC20PaymentClientBase_v1.PaymentOrder memory order,
         bytes memory executionData
